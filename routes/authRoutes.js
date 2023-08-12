9ea1cd7c97625abd3ecf1fac1f9dfaba2e2055aa
Andrei Varcus <andrei.varcus@yahoo.com>
2023-08-12 18:56:58 +0300

    issues with dotenv hope to be solved

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const handleLogin = require('../controllers/loginControler');
 const handleRegister = require('../controllers/registerControler');
 const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 
 const URL_WEB = process.env.URL_WEB;
 
 router.use(express.urlencoded({ extended: false }));
 router.use(cors({ credentials: true, origin: URL_WEB }));
 
-router.get('/', (req, res) => res.send('Loading...'));
+router.get('/', (req, res) => res.send(URL_WEB));
 router.post('/register', handleRegister);
 router.post('/login', handleLogin);
 
