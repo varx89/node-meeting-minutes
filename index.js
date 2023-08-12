6494d491c044bc44ca7167adac166f25de0b9858
Andrei Varcus <andrei.varcus@yahoo.com>
2023-08-12 20:49:23 +0300

    test 404 error still not dissapearing

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 
 const express = require('express');
 const app = express();
-const dbConnect = require('./helpers/connect');
-const routes = require('./routes/authRoutes');
-const cors = require('cors');
+// const dbConnect = require('./helpers/connect');
+// const routes = require('./routes/authRoutes');
+// const cors = require('cors');
 
-const PORT = process.env.APP_PORT;
-const MONGO_URL = process.env.MONGO_URL || 8000;
+// const PORT = process.env.APP_PORT || 8000;
+const PORT = 8000;
+// const MONGO_URL = process.env.MONGO_URL;
 
-dbConnect(MONGO_URL);
+// dbConnect(MONGO_URL);
 
 app.use(express.json());
 
